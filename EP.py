5ce441067c4a76248a6fb7cfa71259591e56fa63
GustavovPierre <38561106+GustavovPierre@users.noreply.github.com>
2018-04-23 19:11:22 -0300

    Add files via upload

---
+++ EP.py
@@ -0,0 +1,66 @@
+#menu de iniciação
+estoque = {}
+escolha = 10
+while escolha != 0:
+    
+    print("0 - sair")
+    print("1 - Adicionar item")
+    print("2 - Remover item")
+    print("3 - alterar item")
+    print("4 - imprimir estoque")
+    escolha = int(input('Faça sua escolha:'))
+    if escolha == 0:
+        print('Até mais')
+    if escolha == 1:
+        x = str(input('nome do produto:'))
+        
+        if x not in estoque:
+            estoque[x] = {}         #se o produto não estiver no estoque, ele deve ser colocado
+            a = int(input('quantidade inicial:'))
+            if a < 0:
+                print('numero invalido')
+            else:
+                b = float(input('valor do produto:'))
+                estoque[x]["quantidade"] = a
+                estoque[x]["valor"] = b
+    
+        else:
+            print('Produto já cadastrado')       #Se já estiver no estoque,avisa que ele já está cadastrado
+       
+    if escolha == 2:
+        w = str(input('Qual produto deseja remover?'))
+        if w not in estoque:
+            print('produto não encontrado')
+        if w in estoque:
+            del estoque[w]
+        
+        
+    if escolha == 3:
+        print("1 - alterar quantidade")
+        print("2 - alterar valor")
+        escolhaAlterar = int(input("Faca sua escolha: "))
+        
+        if escolhaAlterar == 1:
+           produto = str(input("Escolha o produto:"))
+           print(estoque)
+           if produto in estoque:
+               nova_quantidade = int(input("Nova quantidade:"))
+               estoque[produto]["quantidade"] = nova_quantidade + a
+               a = estoque[produto]["quantidade"]
+        if escolhaAlterar == 2:
+            produto = str(input("Escolha o produto:"))
+            if produto in estoque:
+                novo_valor = int(input("Novo valor:"))
+                estoque[produto]["valor"] = novo_valor
+        
+    if escolha == 4:
+        print(estoque)
+        
+
+
+
+
+
+
+
+
